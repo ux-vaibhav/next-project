2657c4358f81b181fcb05e90dc9bae36fa20972b
Vaibhav <ux.vaibhav@gmail.com>
2023-10-28 12:46:17 +0530

    folder Changed

--- src/app/authentication/components/user-auth-form.tsx
+++ src/app/authentication/components/user-auth-form.tsx
@@ -27,7 +27,9 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
     <div className={cn("grid gap-6", className)} {...props}>
       <form onSubmit={onSubmit}>
         <div className="grid gap-2">
-          <div className="grid gap-1">
+
+           
+             <div className="grid gap-1">
             <Label className="sr-only" htmlFor="email">
               Email
             </Label>
--- src/app/calldisposition/components/call-disposition.tsx
+++ src/app/calldisposition/components/call-disposition.tsx
@@ -57,30 +57,31 @@ export function CallDispositionForm() {
   });
 
   function onSubmit(data: z.infer<typeof FormSchema>) {
+    setIsLoading(true);
 
-  setIsLoading(true);
-  
-  fetch('https://dummyjson.com/products/add', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({
-    title:data.category,
-    /* other product data */
-  })
-})
-.then(res => res.json())
-.then((res)=>{
-  setIsLoading(false);
-  toast({
-    title: `You submitted the following values`,
-    description: (
-      <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-        <code className="text-white">{JSON.stringify(res.title, null, 2)}</code>
-      </pre>
-    ),
-  });
-});
- }
+    fetch("https://dummyjson.com/products/add", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        title: data.category,
+        /* other product data */
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        setIsLoading(false);
+        toast({
+          title: `You submitted the following values`,
+          description: (
+            <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+              <code className="text-white">
+                {JSON.stringify(res.title, null, 2)}
+              </code>
+            </pre>
+          ),
+        });
+      });
+  }
 
   return (
     <>
