012d61dc04f43bd892db6377e99a64d815a9b6a9
Vaibhav <ux.vaibhav@gmail.com>
2023-10-28 14:02:35 +0530

    husky check

--- src/app/authentication/components/user-auth-form.tsx
+++ src/app/authentication/components/user-auth-form.tsx
@@ -1,21 +1,18 @@
 'use client';
-
 import * as React from 'react';
-
 import { cn } from '@/lib/utils';
-
 import { Button } from '@/components/ui/button';
 import { Label } from '@radix-ui/react-label';
 import { Input } from '@/components/ui/input';
 import { Icons } from '@/components/ui/icons';
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
-
 export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
 
   async function onSubmit(event: React.SyntheticEvent) {
     event.preventDefault();
+
     setIsLoading(true);
 
     setTimeout(() => {
@@ -26,7 +23,6 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
   return (
     <div className={cn('grid gap-6', className)} {...props}>
       <form onSubmit={onSubmit}>
-      
         <div className="grid gap-2">
           <div className="grid gap-1">
             <Label className="sr-only" htmlFor="email">
